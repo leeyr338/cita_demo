1195a00f6cd6f5f442d9fb4ee591fc5fad4fb3a1
leeyr <leeyr@cryptape.com>
2018-07-25 15:29:43 +0800

    more code for jsonrpc and auth

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -2,23 +2,24 @@
 extern crate auth;
 extern crate protobuf;
 extern crate grpc;
+extern crate rand;
 
-//extern crate futures_cpupool;
-
-use auth::interface::auth::*;
 use grpc::Server;
 use auth::interface::auth_grpc::authority;
 use auth::interface::auth_grpc::*;
 
 use std::thread;
+use rand::Rng;
 
-struct Auth;
+struct Auth {
+    inner_id : u32,
+}
 
 impl authority for Auth {
-    fn auth (&self, o : ::grpc::RequestOptions, p : auth::Transation) -> ::grpc::SingleResponse<auth::State> {
+    fn auth (&self, _o : ::grpc::RequestOptions, p : auth::Transation) -> ::grpc::SingleResponse<auth::State> {
         let mut resp = auth::State::new();
         let req = p.get_tx();
-        let res = format!("auth tx : {}", req);
+        let res = format!("{} --> Auth ID_{} respone ok", req, self.inner_id);
 
         resp.set_state(res);
         grpc::SingleResponse::completed(resp)
@@ -30,9 +31,9 @@ fn main() {
 
     let mut server = grpc::ServerBuilder::new_plain();
 
-    server.http.set_addr("0.0.0.0:30303");
+    server.http.set_addr("0.0.0.0:30303").expect("cannot setup auth service");
     server.http.set_cpu_pool_threads(4);
-    server.add_service(authorityServer::new_service_def(Auth));
+    server.add_service(authorityServer::new_service_def(Auth{inner_id : rand::thread_rng().gen::<u32>()}));
     let _server : Server = server.build().expect("server");
 
     loop {
--- jsonrpc/src/main.rs
+++ jsonrpc/src/main.rs
@@ -1,30 +1,66 @@
+
 #![deny(warnings)]
 extern crate hyper;
 extern crate pretty_env_logger;
+extern crate auth;
+extern crate protobuf;
+extern crate grpc;
+// extern crate futures;
 
-use hyper::{Body, Response, Server};
+use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
+
+use hyper::{Body, Response, Server, Method, StatusCode};
 use hyper::service::service_fn_ok;
 use hyper::rt::{self, Future};
 
-static PHRASE : &'static [u8] = b"Hello world, leeyr";
+// // use futures::future;
+use auth::interface::auth_grpc::authority;
+use auth::interface::auth_grpc::*;
 
 fn main() {
+    println!("Running jsonrpc service...", );
     pretty_env_logger::init();
 
-    let addr = ([0,0,0,0], 3000).into();
+    let addr = ([127, 0, 0, 1], 3000).into();
 
-    let new_service = || {
-        service_fn_ok(|_| {
-            Response::new(Body::from(PHRASE))
+    let counter = Arc::new(AtomicUsize::new(0));
+
+    let new_service = move || {
+        let client = authorityClient::new_plain("http://auth", 6666, Default::default()).unwrap();
+        let counter = counter.clone();
+
+        service_fn_ok(move |req| {
+            let count = counter.fetch_add(1, Ordering::AcqRel);
+            let mut response = Response::new(Body::empty());
+
+            match (req.method(), req.uri().path()) {
+                (&Method::GET, "/") => {
+                    *response.body_mut() = Body::from("...welcome to cita demo...");
+                }
+                (&Method::POST, "/tx") => {
+                    let mut tx = auth::Transation::new();
+
+                    tx.set_tx("new transation".to_string());
+                    let resp = client.auth(grpc::RequestOptions::new(), tx);
+                    match resp.wait() {
+                        Ok(rsp) => *response.body_mut() = Body::from(format!("Request #{} : {:?}", count, rsp.1.get_state())),
+                        Err(e) => *response.body_mut() = Body::from(format!("Request #{} : {:?}", count, e)),
+                    }
+
+                }
+                _ => {
+                    *response.status_mut() = StatusCode::NOT_FOUND;
+                }
+            }
+            response
         })
     };
 
     let server = Server::bind(&addr)
-                 .serve(new_service)
-                 .map_err(|e| eprintln!("server error {}", e));
+        .serve(new_service)
+        .map_err(|e| eprintln!("server error: {}", e));
 
+    println!("Listening on http://{}", addr);
 
-
-    println!("lisenting on http://{}", addr);
     rt::run(server);
 }
